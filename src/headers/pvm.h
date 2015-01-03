0e1296dab7f1e0637b93ec98f84636d8550a41b2
Victor <digitloft@gmail.com>
2015-01-03 21:06:17 +0100

    A couple of fixes.

--- src/headers/pvm.h
+++ src/headers/pvm.h
@@ -7,11 +7,12 @@
 unsigned int  memory[MEMSIZE] = {0};
 unsigned int  reg[REGISTERS] = {0};
 unsigned char mode, halt, inst, x, y, k, psp=0;
-unsigned int  pc, mmmm, nnn, exit_code = 0;
+unsigned int  pc, mmmm, nnn, exit_code = EXIT_SUCCESS;
 unsigned long opcode;
 
 unsigned int* X;
 unsigned int pc_stack[0xFF] = {0};
 unsigned int arrayX[0xF] = {0};
+char* PROGNAME = NULL;
 
 typedef char FLAG;
