2d14203586cb98d8050c1c8693f0c0a24be93bec
Victor <digitloft@gmail.com>
2015-01-03 22:52:38 +0100

    A couple of fixes.

--- src/headers/pasm.h
+++ src/headers/pasm.h
@@ -1,10 +1,12 @@
 #include <stdio.h>
-#define  MAXLINES 65535
+#define EXIT_ASM_ERROR 2
+#define MAXLINES 0xFFFF
+#define MAXLABELSIZE 0xFFF
 #define __PASM_VERSION__ "0.1"
 
 typedef struct {
     unsigned int address;
-    char *label;
+    char label[MAXLABELSIZE];
 } Label;
 
 Label lookup[MAXLINES];
@@ -12,7 +14,6 @@ size_t LOOKUP_PT = 0;
 
 FILE *fpasm, *fpbin;
 char *words[MAXLINES];
+char* PROGNAME = NULL;
 
-void die(char*);
-void pass1();
-void pass2();
+unsigned int linenum = 0;
