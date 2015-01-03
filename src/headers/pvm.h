826e72e9c9cef776e4f9290cf8220c8f98096677
Victor <digitloft@gmail.com>
2015-01-03 20:26:00 +0100

    Create pvm.h

---
+++ src/headers/pvm.h
@@ -0,0 +1,17 @@
+// P Virtual Machine - header file
+#define MEMSIZE 65535
+#define REGISTERS 16
+#define DEBUG 0
+#define __PVM_VERSION__ "0.1"
+
+unsigned int  memory[MEMSIZE] = {0};
+unsigned int  reg[REGISTERS] = {0};
+unsigned char mode, halt, inst, x, y, k, psp=0;
+unsigned int  pc, mmmm, nnn, exit_code = 0;
+unsigned long opcode;
+
+unsigned int* X;
+unsigned int pc_stack[0xFF] = {0};
+unsigned int arrayX[0xF] = {0};
+
+typedef char FLAG;
